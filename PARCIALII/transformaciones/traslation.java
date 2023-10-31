fad10622e9beb83c98d723f872756185d665089c
YazminUrzua <urzua.yazmin@gmail.com>
2023-10-30 23:30:13 -0600

    update transformations
    fix and update transformations classes

--- PARCIALII/transformaciones/traslation.java
+++ PARCIALII/transformaciones/traslation.java
@@ -1,148 +1,123 @@
 package PARCIALII.transformaciones;
-import PARCIALII.transformaciones.traslationC;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.geom.Path2D;
 import java.awt.image.BufferedImage;
-
 public class traslation extends JPanel implements ActionListener {
-    
     private BufferedImage buffer;
     private Graphics2D graPixel;
-    private static final int WIDTH = 800;
-    private static final int HEIGHT = 800;
-
-    private Color backgroundColor = Color.WHITE;
-    private Color ghostColor1 = Color.CYAN;
-    private Color ghostColor2 = Color.MAGENTA;
-
-    private double[][] ghostVertices1;
-    private double[][] ghostVertices2;
- private int sizePh = 15;
-
-    private double translateX2 = 200;
-    private double translateY2 = 0;
-    
-
-
-       private double translateX1 = -200;
-    private double translateY1 = 0;
+    private static final int WIDTH = 500;
+    private static final int HEIGHT = 500;
+    private static final int RECT_WIDTH = 80;
+    private static final int RECT_HEIGHT = 40;
+    private double translateX = 0;
+    private double translateY = 0;
+    private double[][] rectangleVertices;
+    private Color backgroundColor = Color.BLACK;
+    private Color fillColor = Color.magenta;
+    private boolean increasing = true;
     public traslation() {
         buffer = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
         graPixel = buffer.createGraphics();
         Timer timer = new Timer(100, this);
         timer.start();
-
-         translateX1 = -200;
-     translateY1 = 300;
-
-     
-     translateX2 = 200;
-     translateY2 = 200;
- // Define los vértices para el rectángulo (en lugar del triángulo)
- ghostVertices1 = new double[][] {
-    {-sizePh, -sizePh, 1},
-    {sizePh, -sizePh, 1},
-    {sizePh, sizePh, 1},
-    {-sizePh, sizePh, 1}
-};
-
-ghostVertices2 = new double[][] {
-    {-sizePh, -sizePh, 1},
-    {sizePh, -sizePh, 1},
-    {sizePh, sizePh, 1},
-    {-sizePh, sizePh, 1}
-};
-}
-
+        rectangleVertices = new double[][]{
+                {-RECT_WIDTH / 2, -RECT_HEIGHT / 2, 1}, // P1
+                {RECT_WIDTH / 2, -RECT_HEIGHT / 2, 1},  // P2
+                {RECT_WIDTH / 2, RECT_HEIGHT / 2, 1},   // P3
+                {-RECT_WIDTH / 2, RECT_HEIGHT / 2, 1}   // P4
+        };
+    }
     public void putPixel(int x, int y, Color c) {
         buffer.setRGB(x, y, c.getRGB());
     }
-
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
         graPixel.setColor(backgroundColor);
         graPixel.fillRect(0, 0, WIDTH, HEIGHT);
-
-        // Dibuja el primer fantasma
-        graPixel.setColor(ghostColor1);
-        drawGhost(ghostVertices1, translateX1, translateY1);
-
-        // Dibuja el segundo fantasma
-        graPixel.setColor(ghostColor2);
-        drawGhost(ghostVertices2, translateX2, translateY2);
-
+        double[][] translationMatrix = {
+                {1, 0, translateX},
+                {0, 1, translateY},
+                {0, 0, 1}
+        };
+        int[] xPoints = new int[rectangleVertices.length];
+        int[] yPoints = new int[rectangleVertices.length];
+        for (int i = 0; i < rectangleVertices.length; i++) {
+            double[] transformedVertex = multiplyMatrixAndPoint(translationMatrix, rectangleVertices[i]);
+            xPoints[i] = (int) (transformedVertex[0] + (WIDTH / 2));
+            yPoints[i] = (int) (transformedVertex[1]);
+        }
+        fillPolygon(xPoints, yPoints, fillColor);
         g.drawImage(buffer, 0, 0, this);
     }
-
-    public void drawGhost(double[][] vertices, double translateX, double translateY) {
-        Path2D.Double path = new Path2D.Double();
-        path.moveTo(vertices[0][0] + translateX, vertices[0][1] + translateY);
-
-        for (int i = 1; i < vertices.length; i++) {
-            path.lineTo(vertices[i][0] + translateX, vertices[i][1] + translateY);
-        }
-
-        path.closePath();
-        graPixel.fill(path);
-
-        int eyeX1 = (int) (vertices[0][0] + translateX - 1);
-        int eyeY1 = (int) (vertices[0][1] + translateY - 6);
-
-        int eyeX2 = (int) (vertices[0][0] + translateX + 15);
-        int eyeY2 = (int) (vertices[0][1] + translateY - 6);
-        int eyeRadius = 4;
-        int head = (int) (vertices[0][0] +translateX);
-        int headY = (int) (vertices[0][1] +translateY-10);
-        int headRadius =15;
-        graPixel.setColor(ghostColor1);
-        graPixel.fillOval(head, headY, headRadius * 2, headRadius * 2);
-
-        graPixel.setColor(Color.WHITE);
-        graPixel.fillOval(eyeX1, eyeY1, eyeRadius * 2, eyeRadius * 2);
-        graPixel.fillOval(eyeX2, eyeY2, eyeRadius * 2, eyeRadius * 2);
-
-       graPixel.setColor(Color.BLACK);
-       graPixel.drawOval(eyeX1, eyeY1, eyeRadius * 2, eyeRadius * 2);
-       graPixel.drawOval(eyeX2, eyeY2, eyeRadius * 2, eyeRadius * 2);
-
-       
-        
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
-        animateGhosts();
-        drawGhost(ghostVertices1, translateX1, translateY1);
+        if (increasing) {
+            translateX += 5;
+            translateY += 5;
+            if (translateX >= WIDTH / 2) {
+                increasing = false;
+            }
+        } else {
+            translateX -= 5;
+            translateY -= 5;
+            if (translateX <= -WIDTH / 2) {
+                increasing = true;
+            }
+        }
         repaint();
     }
-
-    public void animateGhosts() {
-        // Animación del primer fantasma
-        translateX1 += 5;
-        if (translateX1 > WIDTH)
-            translateX1 = -40;
-
-        // Animación del segundo fantasma
-        translateX2 -= 5;
-        if (translateX2 < -40)
-            translateX2 = WIDTH;
-    }
-
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Ghost Animation");
+        JFrame frame = new JFrame("Translation Animation Diagonal");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(WIDTH, HEIGHT);
-
         traslation animation = new traslation();
         frame.add(animation);
-        
-
         frame.setVisible(true);
     }
-
+    private double[] multiplyMatrixAndPoint(double[][] matrix, double[] point) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        double[] result = new double[rows];
+        for (int i = 0; i < rows; i++) {
+            double sum = 0;
+            for (int j = 0; j < cols; j++) {
+                sum += matrix[i][j] * point[j];
+            }
+            result[i] = sum;
+        }
+        return result;
+    }
+//usamos bresenham y scanline para rellenar y trazar
+    private void fillPolygon(int[] xPoints, int[] yPoints, Color fillColor) {
+        int minY = Integer.MAX_VALUE;
+        int maxY = Integer.MIN_VALUE;
+        for (int y : yPoints) {
+            if (y < minY) minY = y;
+            if (y > maxY) maxY = y;
+        }
+        for (int y = minY; y <= maxY; y++) {
+            int x1 = Integer.MAX_VALUE;
+            int x2 = Integer.MIN_VALUE;
+            for (int i = 0; i < xPoints.length; i++) {
+                int j = (i + 1) % xPoints.length;
+                int xi = xPoints[i];
+                int xj = xPoints[j];
+                int yi = yPoints[i];
+                int yj = yPoints[j];
+                if ((yi <= y && yj > y) || (yj <= y && yi > y)) {
+                    int x = (int) (xi + (double) (y - yi) / (double) (yj - yi) * (xj - xi));
+                    if (x < x1) x1 = x;
+                    if (x > x2) x2 = x;
+                }
+            }
+            if (x1 <= x2) {
+                for (int x = x1; x <= x2; x++) {
+                    putPixel(x, y, fillColor);
+                }
+            }
+        }
+    }
 }
